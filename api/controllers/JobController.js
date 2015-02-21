242c6af703d7cffb2b20107cff2086ce1a9eb11f
James Wu <jwu@efunda.com>
2015-02-21 13:02:48 -0800

    created user and job models

---
+++ api/controllers/JobController.js
@@ -0,0 +1,11 @@
+/**
+ * JobController
+ *
+ * @description :: Server-side logic for managing Jobs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
